16b81e642f781f07c8ab90bc7093d71f43a83882
bubber28 <rubemardigital@gmail.com>
2026-03-13 17:55:41 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
+import "./globals.css";
+
 export const metadata = {
   title: "M.A.C",
-  description: "Painel do agente inteligente"
-}
+  description: "Painel do agente inteligente",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
+  );
 }
