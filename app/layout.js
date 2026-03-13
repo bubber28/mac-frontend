1da4e9f1a62c0198177da4a19c9c051824b98af4
bubber28 <rubemardigital@gmail.com>
2026-03-12 22:30:03 -0300

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "M.A.C",
+  description: "Painel do agente inteligente"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
