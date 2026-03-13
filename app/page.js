27f19c2fe06a8300e688558442848612a5e7c8f1
bubber28 <rubemardigital@gmail.com>
2026-03-12 22:25:14 -0300

    Add Home component with main content

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:40,fontFamily:"Arial"}}>
+      <h1>M.A.C</h1>
+      <p>Painel do agente inteligente</p>
+    </main>
+  )
+}
